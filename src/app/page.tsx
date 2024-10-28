bfd02db93993da162cc6b1e0f541e7ab021869a5
undg <und3rdg@gmail.com>
2024-10-28 21:43:36 +0000

    extract status to custom hook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,9 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { socket } from "~/server/socket";
+import { useWsStatus } from "~/server/use-ws-status";
 
 export default function Home() {
-  const [isConnected, setIsConnected] = useState(false);
-  const [transport, setTransport] = useState("N/A");
-
-  useEffect(() => {
-    if (socket.connected) {
-      onConnect();
-    }
-
-    function onConnect() {
-      setIsConnected(true);
-      setTransport(socket.io.engine.transport.name);
-
-      socket.io.engine.on("upgrade", (transport) => {
-        setTransport(transport.name);
-      });
-    }
-
-    function onDisconnect() {
-      setIsConnected(false);
-      setTransport("N/A");
-    }
-
-    socket.on("connect", onConnect);
-    socket.on("disconnect", onDisconnect);
-
-    return () => {
-      socket.off("connect", onConnect);
-      socket.off("disconnect", onDisconnect);
-    };
-  }, []);
+  const {isConnected, transport} = useWsStatus()
 
   return (
     <div>
