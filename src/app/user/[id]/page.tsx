452cdb15356313c60e0e9b93b1e14ce92183a77d
undg <und3rdg@gmail.com>
2024-10-28 22:07:50 +0000

    working messaging system

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -1,16 +1,28 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { ChatBubble, ChatBubbleMessage } from "~/components/ui/chat/chat-bubble";
-import { ChatInput } from "~/components/ui/chat/chat-input";
+import { useParams } from "next/navigation";
+import { type FormEvent, useEffect, useState } from "react";
+import {
+  ChatBubble,
+  ChatBubbleMessage,
+  ChatBubbleTimestamp,
+} from "~/components/ui/chat/chat-bubble";
+import { Input } from "~/components/ui/input";
 import { type Chat, type User } from "~/server/db";
 import { socket } from "~/server/socket";
+import { useWsStatus } from "~/server/use-ws-status";
 
 export default function Home() {
-  const [isConnected, setIsConnected] = useState(false);
-  const [transport, setTransport] = useState("N/A");
+  const params = useParams();
+  const userId = Number(params.id);
+  // Naive shortcut. Hard assumption that there are only 2 users in app
+  const sendTo: number = userId === 1 ? 2 : 1;
+
+  const { isConnected, transport } = useWsStatus();
+
   const [users, setUsers] = useState<User[]>();
   const [chats, setChats] = useState<Chat[]>([]);
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
     socket.on("users", (users: User[]) => {
@@ -33,21 +45,41 @@ export default function Home() {
   }, []);
 
   // Send new message
-  const sendMessage = (messageData: Chat) => () => {
+  const sendMessage = (messageData: Chat) => (e: FormEvent) => {
+    e.preventDefault();
     socket.emit("send-message", messageData);
+    setMessage("");
   };
 
   return (
     <div>
       <p>Status: {isConnected ? "connected" : "disconnected"}</p>
       <p>Transport: {transport}</p>
-      <button onClick={sendMessage({message: 'test message', to: 2, from: 1})}>Send</button>
-      <ChatBubble variant='received'>
-        <ChatBubbleMessage color="pink">
-          afasdfaf
-        </ChatBubbleMessage>
-      </ChatBubble>
-      <ChatInput />
+      {users?.map((u) => (
+        <div key={u.id}>
+          {u.id} {u.name} {u.gender}
+        </div>
+      ))}
+      {chats.map((msg) => (
+        <div key={msg.id}>
+          <ChatBubbleTimestamp timestamp={msg.createdAt ?? ''} />
+          <ChatBubble variant={userId === msg.from ? "sent" : "received"}>
+            <ChatBubbleMessage>{msg.message}</ChatBubbleMessage>
+          </ChatBubble>
+        </div>
+      ))}
+      <form onSubmit={sendMessage({ message, to: sendTo, from: userId })}>
+        <Input
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter" && !e.shiftKey) {
+              e.preventDefault();
+              e.currentTarget.form?.requestSubmit();
+            }
+          }}
+        />
+      </form>
     </div>
   );
 }
